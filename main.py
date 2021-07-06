f7bf676a1053ea50ee8303ed47271e90ab6f6d8b
Saurabh Kumar <30611169+saurabh59@users.noreply.github.com>
2021-07-07 01:35:35 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+import budget
+from budget import create_spend_chart
+
+food = budget.Category("Food")
+food.deposit(1000, "initial deposit")
+food.withdraw(10.15, "groceries")
+food.withdraw(15.89, "restaurant and more food for dessert")
+print(food.get_balance())
+clothing = budget.Category("Clothing")
+food.transfer(50, clothing)
+clothing.withdraw(25.55)
+clothing.withdraw(100)
+auto = budget.Category("Auto")
+auto.deposit(1000, "initial deposit")
+auto.withdraw(15)
+
+print(food)
+print(clothing)
+
+print(create_spend_chart([food, clothing, auto]))
